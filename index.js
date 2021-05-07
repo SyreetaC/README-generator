d885ec20d4e51bc428f7f09b4a9c02ba26dc6ad1
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-07 18:40:03 +0100

    added questions and needed packages

--- index.js
+++ index.js
@@ -1,13 +1,101 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions
+const basicProjectQuestions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "list",
+    choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"],
+    name: "license",
+    message: "Select a license for your project",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the project's description?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How can people contribute to the project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Are there any tests for this project?",
+  },
+  {
+    type: "input",
+    name: "gitHub",
+    message: "What is your gitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your e-mail address?",
+  },
+];
 
-// TODO: Create a function to write README file
-const writeToFile = (fileName, data) => {};
+const installationRequirements = [
+  {
+    type: "confirm",
+    name: "installation",
+    message: "Are there any installation requirements?",
+  },
+];
 
-// TODO: Create a function to initialize app
-const init = () => {};
+const installationCodeInput = [
+  {
+    type: "input",
+    name: "installationCode",
+    message: "Type your installation code directly below:",
+  },
+];
+
+const installationRequirementConfirm = [
+  {
+    type: "confirm",
+    name: "furtherInstallation",
+    message: "Are there any further installation requirements?",
+  },
+];
+
+const usageRequirementsConfirm = [
+  {
+    type: "confirm",
+    name: "usage",
+    message: "Are there any usage information?",
+  },
+];
+
+const usageInfoInput = [
+  {
+    type: "input",
+    name: "usageInfo",
+    message: "Type your usage information below:",
+  },
+];
+
+const furtherUsageInput = [
+  {
+    type: "confirm",
+    name: "furtherUsage",
+    message: "Are there any further usage information?",
+  },
+];
+
+// Function to write READme file
+
+// Create a function to collect data using inquirer
+
+// Function to initialize app
 
 // Function call to initialize app
 init();
